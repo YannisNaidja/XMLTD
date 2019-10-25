a98d774e25ba4860d0e4002207893a1cc97a5b3c
basil.dalie@etu.umontpellier.fr <basil.dalie@etu.umontpellier.fr@d20.fdsetu.infra.umontpellier.fr>
2019-10-25 10:29:27 +0200

    Updated repository structure

--- TD3/exo1/q3.sql
+++
@@ -1,6 +0,0 @@
-# -- /presse/journal/article[auteur=/presse/journalistes[@idJ='j1']]
-SELECT *
-FROM ARTICLE ART
-JOIN AUTEURATT AUT ON ART.Target = AUT.Source
-JOIN JOURNALISTEIDATT JID
-WHERE JID.txtval = 'j1'
