b60b805015d99572fe59f0dea99fb6834f03d22f
basil.dalie@etu.umontpellier.fr <basil.dalie@etu.umontpellier.fr@n6.fdsetu.infra.umontpellier.fr>
2019-12-04 10:49:08 +0100

    Fixed indentation of products.component

--- Partie2/angular/Drinky/src/app/products/products.component.ts
+++ Partie2/angular/Drinky/src/app/products/products.component.ts
@@ -4,31 +4,31 @@ import { Product } from '../product';
 import { ProductFactory } from '../product-factory';
 
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+    selector: 'app-products',
+    templateUrl: './products.component.html',
+    styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-  private products : any[] = new Array();
-  
-  constructor(private productsService : ProductsService) { }
-
-  ngOnInit() {
-      this.productsService.getProducts().subscribe(p => {
-	  this.products = p;
-
-	  this.products.sort(function(a, b) {
-	      let initialA = a.product_name.toLowerCase()[0];
-	      let initialB = b.product_name.toLowerCase()[0];
-
-	      if (initialA < initialB) {
-		  return -1;
-	      } else if (initialB < initialA) {
-		  return 1;
-	      }
-
-	      return 0;
-	  });
-      });
-  }
+    private products : any[] = new Array();
+    
+    constructor(private productsService : ProductsService) { }
+    
+    ngOnInit() {
+	this.productsService.getProducts().subscribe(p => {
+	    this.products = p;
+	    
+	    this.products.sort(function(a, b) {
+		let initialA = a.product_name.toLowerCase()[0];
+		let initialB = b.product_name.toLowerCase()[0];
+		
+		if (initialA < initialB) {
+		    return -1;
+		} else if (initialB < initialA) {
+		    return 1;
+		}
+		
+		return 0;
+	    });
+	});
+    }
 }
